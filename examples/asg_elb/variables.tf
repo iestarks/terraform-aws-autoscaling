401491af6ddd9743dafc1bb87f14ceef572cc88d
Irving <ncsu.ee2017@gmail.com>
2021-01-20 13:33:54 -0500

    repaired asg_lc

--- examples/asg_elb/variables.tf
+++ examples/asg_elb/variables.tf
@@ -1,9 +1,14 @@
 variable "elbname" {
   description = "ELB Security Group Name"
   type        = string
-  default = "autoscaling-elb"
+  default = "autoscaling-elb2"
 }
 
+variable "create_lc" {
+  description = "Whether to create launch configuration"
+  type        = bool
+  default     = true
+}
 
 
 variable "appname" {
@@ -18,4 +23,8 @@ variable "appsgname" {
   default = "usbank-appserv"
 }
 
-
+variable "recreate_asg_when_lc_changes" {
+  description = "Whether to recreate an autoscaling group when launch configuration changes"
+  type        = bool
+  default     = false
+}
